0c562c8b8cdb00afd9cbacf6f9750af9ba9996b6
liulin6631 <62631441+liulin6631@users.noreply.github.com>
2021-03-30 14:29:36 +0800

    Update op.js

--- op.js
+++ op.js
@@ -1,6 +1,39 @@
-var body = $request.body;
-var reg = new RegExp("amount=50", "g");
-var reg1 = new RegExp("amount=100", "g");
-body = body.replace(reg, "amount=1");
-body = body.replace(reg1, "amount=1");
-$done({body});
+function formatDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2)
+        month = '0' + month;
+    if (day.length < 2)
+        day = '0' + day;
+
+    return [year, month, day].join('');
+}
+
+let requestHeaders = $request.headers
+let requestBody = $request.body
+
+let nonceStr = requestBody.substr(10)
+let date = formatDate()
+let body = {
+    "message": "success",
+    "xianshi": requestHeaders["xianshi"],
+    "is_pay": false,
+    "use_info": {
+        "date": `${date}`,
+        "used": 1,
+        "ins_number": 0,
+        "total": 16
+    },
+    "nonce_str": nonceStr
+}
+
+console.log(body)
+
+$done({
+    response: {
+        body: JSON.stringify(body)
+    }
+})
